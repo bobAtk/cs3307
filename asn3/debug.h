87b13e5d4402b8912e4008340ca3ae4385ad4268
bobAtk <stempower@gmail.com>
2014-12-01 20:38:53 -0500

    Create debug.h
    For logger on GAUL.

---
+++ asn3/debug.h
@@ -0,0 +1,10 @@
+//HEADER AS I WANT TO USE THIS STUFF IN TEXT UI
+//Made for CS3307a Final Project
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+//Function prototypes of debug.cpp that we want available everywhere
+int bank_printf(bool traceSignal, const char *fmt, ...); 	//Function that prints message (and debug ONLY IF trace is on)
+
+#endif
